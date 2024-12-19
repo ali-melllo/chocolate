8e2b07fd7933d309f37f500daff65ee1559ddb32
ali-melllo <alimelllo32@gmail.com>
2024-12-19 15:39:15 +0330

    head fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import { Metadata } from "next"
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
-import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
 
@@ -33,9 +32,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
       <html lang="en" suppressHydrationWarning>
-        <head>
-
-        </head>
+        <head/>
         <body
           className={cn(
             "min-h-screen bg-background font-[Dana] antialiased",
