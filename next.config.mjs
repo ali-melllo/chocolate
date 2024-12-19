d69789e9170efa0e1ae11f57a892a4ca2d31454e
ali-melllo <alimelllo32@gmail.com>
2024-12-19 15:23:44 +0330

    next config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-        pathname: "**",
-      },
-    ],
-  },
   experimental: {
     appDir: true,
   },
